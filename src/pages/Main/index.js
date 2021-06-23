781a83498dc6e4a91a342f7b3cd378b42a89a60b
audisiofilho <obuzua46@gmail.com>
2021-06-22 21:21:31 -0300

    Estilização de botão loading.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,21 +1,67 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 
-import { FaGithub, FaPlus } from 'react-icons/fa'
+import { FaGithub, FaPlus, FaSpinner } from "react-icons/fa";
 import { Container, Form, SubmitButton } from "./styles";
+import api from "../../services/api";
 
 export default function Main() {
+  const [newRepo, setNewRepo] = useState("");
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  function handleinputChange(e) {
+    setNewRepo(e.target.value);
+  }
+
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      async function submit() {
+        setLoading(true);
+
+        try {
+          const response = await api.get(`repos/${newRepo}`);
+
+          const data = {
+            name: response.data.full_name,
+          };
+
+          setRepos([...repos, data]);
+          setNewRepo("");
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setLoading(false);
+        }
+      }
+
+      submit();
+    },
+    [newRepo, repos]
+  );
+
   return (
     <div>
       <Container>
         <h1>
-          <FaGithub size={25}/>
+          <FaGithub size={25} />
           Meus Repositorios
         </h1>
 
-        <Form onSubmit={()=>{}}>
-          <input type="text" placeholder="Adiconar Repositorios" />
-          <SubmitButton>
-            <FaPlus color="#fff" size={14}/>
+        <Form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adiconar Repositorios"
+            value={newRepo}
+            onChange={handleinputChange}
+          />
+          <SubmitButton Loading={loading ? 1 : 0}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#fff" size={14} />
+            )}
           </SubmitButton>
         </Form>
       </Container>
