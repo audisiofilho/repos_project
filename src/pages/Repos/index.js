7ac77444352a82d42cedec971b4d219602d43e90
audisiofilho <obuzua46@gmail.com>
2021-06-24 10:50:53 -0300

    Exibindo issues e criando paginação

--- src/pages/Repos/index.js
+++ src/pages/Repos/index.js
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from "react";
 
-import { Container, Owner, Loading, BackButton } from "./styles";
-import { FaArrowLeft } from 'react-icons/fa';
+import {
+  Container,
+  Owner,
+  Loading,
+  BackButton,
+  IssuesList,
+  PageActions,
+} from "./styles";
+import { FaArrowLeft } from "react-icons/fa";
 import api from "../../services/api";
 
 export default function Repos({ match }) {
-
   const [repos, setRepos] = useState({});
   const [issues, setIssues] = useState([]);
   const [loading, setloading] = useState(true);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     async function load() {
@@ -17,10 +24,10 @@ export default function Repos({ match }) {
       const [reposData, issuesData] = await Promise.all([
         api.get(`/repos/${nomeRepo}`),
         api.get(`/repos/${nomeRepo}/issues`, {
-          params:{
-            state: 'open',
-            per_page: 5
-          }
+          params: {
+            state: "open",
+            per_page: 5,
+          },
         }),
       ]);
 
@@ -32,28 +39,74 @@ export default function Repos({ match }) {
     load();
   }, [match.params.repos]);
 
-  if(loading){
-    return(
+  useEffect(()=>{
+    async function loadIssue(){
+      const nomeRepo = decodeURIComponent(match.params.repos);
+
+      const response = await api.get(`/repos/${nomeRepo}/issues`, {
+        params:{
+          state: 'open',
+          page,
+          per_page: 5,
+        },
+      });
+
+      setIssues(response.data);
+    }
+
+    loadIssue();
+  }, [page]);
+
+  function handlePage(action){
+    setPage(action === 'back' ? page - 1 : page + 1)
+  }
+
+  if (loading) {
+    return (
       <Loading>
         <h1>Carregando...</h1>
       </Loading>
-
-    )
+    );
   }
 
-  return(
-
+  return (
     <Container>
       <BackButton to="/">
-        <FaArrowLeft color="#000" size={30}/>
+        <FaArrowLeft color="#000" size={30} />
       </BackButton>
       <Owner>
-        <img src={repos.owner.avatar_url} 
-        alt={repos.owner.login}
-        />
+        <img src={repos.owner.avatar_url} alt={repos.owner.login} />
         <h1>{repos.name}</h1>
         <p>{repos.description}</p>
       </Owner>
+
+      <IssuesList>
+        {issues.map((issue) => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+
+                {issue.labels.map((label) => (
+                  <span key={String(label.id)}>{label.name}</span>
+                ))}
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+      </IssuesList>
+
+      <PageActions>
+        <button type="button" onClick={() => handlePage('back')} disabled={page < 2}>
+          Voltar
+        </button>
+        <button type="button" onClick={() => handlePage('next')}>
+          Próxima
+        </button>
+      </PageActions>
     </Container>
   );
 }
